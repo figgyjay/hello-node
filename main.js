0dac69d6152b40ea9e02029b3678a1719c0e9f8a
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-05-30 16:33:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+
+
+console.log("HH    HH        111  111                                         111       dd")
+console.log("HH    HH  eee   111  111  0000    ww       ww   oooo    rr rr    111       dd")
+console.log("HHHHHHHH ee  e  111  111 o    o    ww  w  ww   o    o   rr   r   111   dddddd")
+console.log("HH    HH ee     111  111 o    o     ww   ww    o    o   rr       111  d    dd")
+console.log("HH    HH eeeee  111  111  oooo      ww   ww     oooo    rr       111   dddddd")
